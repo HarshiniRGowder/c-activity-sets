5369e90df17b2bda7e4d9213c47c42afe4e70dd2
unknown <harshinir.bsc23@rvu.edu.in>
2024-02-06 14:34:16 +1300

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int main()
+int main();
 {
     int a,b,c;
     printf("enter two number\n");
     scanf("%d %d",&a,&b);
     c=a+b;
     printf("sum of the two number is: %d\n",c);
+    return 0; 
 
 }
