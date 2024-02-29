c9d7355072ae238c1cc07fb1d31b48c12770e939
unknown <harshinir.bsc23@rvu.edu.in>
2024-02-29 22:24:42 +1300

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -2,13 +2,12 @@
 
 int main();
 {
- int a,b,c;
+   int a,b,c;
     printf("enter two number\n");
     scanf("%d %d",&a,&b);
     c=a+b;
     printf("sum of the two number is: %d\n",c);
     return 0;
-    
-
+     
 }
-   
+ 
