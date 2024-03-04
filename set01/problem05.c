8b3b31398a0a74ed04a479515ee71acfe9151b8e
unknown <harshinir.bsc23@rvu.edu.in>
2024-03-04 14:03:11 +1300

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,60 +1,44 @@
-#include <stdio.h>
-
+#include<stdio.h>
 int input();
-int compare(int a, int b, int c);
-void output(int a, int b, int c, int largest);
-
+int compare(int a,int b,int c);
+void output(int a,int b,int largest,int c);
 int main()
 {
-    int num1, num2, num3, largest;
-    num1 = input();
-    num2 = input();
-    num3 = input();
-
-    largest = compare(num1, num2, num3);
-
-    output(num1, num2, num3, largest);
-
+    int a,b,c,largest;
+    a=input();
+    b=input();
+    c=input();
+    
+    largest=compare(a,b,c);
+    output(a,b,c,largest);
     return 0;
 }
-
 int input()
 {
-    int num;
-    printf("Enter a number: \n");
-    scanf("%d", &num);
-    return num;
+    int x;
+    printf("enter two number:");
+    scanf("%d",&x);
+    return x;
 }
-
-int compare(int a, int b, int c)
-
-   /* int largest = a;
-    if (b > largest)
-    {
-        largest = b;
-    }
-
-    if (c > largest)
-    {
-        largest = c;
-    }
-    return largest;*/
-
+int compare(int a , int b,int c)
 {
-   if( a>=b && a>=c )     
-   {
+    int largest;
+    if(a>=b&&a>=c)
+  {
+    largest=a;
     return a;
-   }
-   else if (b>=a && b>=c)
-   {
-    return b;
-   }
-   else 
-   {
+  }
+  if(b>=a&&b>=c)
+  {
+    largest=b;
+    return a;
+  }
+  else{
+    largest=c;
     return c;
-   }
+  }
 }
-void output(int a, int b, int c, int largest)
+void output(int a,int b,int c, int largest)
 {
-    printf("largst of %d,%d and %d is %d\n", a, b, c, largest);
+    printf("the largest of %d,%d and %d is %d\n",a,b,c,largest);
 }
